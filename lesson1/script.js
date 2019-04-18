3a8e005425865e93aab8e31af26ad78cdeb908a5
NikitaV <varfol007@gmail.com>
2019-04-18 12:23:56 +0300

    First try

---
+++ lesson1/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let money = prompt('Ваш бюджет на месяц'),
+    time = prompt('Введите дату в формате YYYY-MM-DD');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpanses:{},
+    income:{},
+    savings: false
+}
+
+let mandExpenses = prompt('Введите обязательную статью расходов в этом месяце'),
+    howMuch = prompt('Во сколько обойдется?');
+
+appData.expenses[mandExpenses] = howMuch;
+
+    mandExpenses = prompt('Введите обязательную статью расходов в этом месяце');
+    howMuch = prompt('Во сколько обойдется?');
+
+appData.expenses[mandExpenses] = howMuch;
+
+let budget_one_day = +money/30;
+
+console.log('Бюджет на 1 день =', budget_one_day);
