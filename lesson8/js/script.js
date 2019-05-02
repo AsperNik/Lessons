fe92c33d6f6c8e75466a60da728133f881f74357
NikitaV <varfol007@gmail.com>
2019-05-02 10:48:02 +0300

    Scroll

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -83,7 +83,29 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     }
+    let links = document.querySelectorAll('a[href*="#"]');
 
+    for (let link of links) {
+        link.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const blockID = link.getAttribute('href');
+        console.log(blockID);
+        if (blockID === '#price') {
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+              });
+        } else {
+            document.querySelector(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+            });
+        }
+        
+      });
+    }
+   
     setClock('timer', deadline);
 });
 
