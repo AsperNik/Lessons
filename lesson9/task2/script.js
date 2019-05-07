696b02900d74b34820175825e77c2b4489be688b
NikitaV <varfol007@gmail.com>
2019-05-07 18:11:52 +0300

    FT

--- lesson9/task2/script.js
+++ lesson9/task2/script.js
@@ -1,31 +1,8 @@
-function main(){
-'use strict';
-
-let money = prompt('Ваш бюджет на месяц'),
-    time = prompt('Введите дату в формате YYYY-MM-DD');
-
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpanses:{},
-    income:[],
-    savings: false
-};
-
-let mandExpenses = prompt('Введите обязательную статью расходов в этом месяце'),
-    howMuch = prompt('Во сколько обойдется?');
-
-appData.expenses[mandExpenses] = howMuch;
-
-    mandExpenses = prompt('Введите обязательную статью расходов в этом месяце');
-    howMuch = prompt('Во сколько обойдется?');
-
-appData.expenses[mandExpenses] = howMuch;
-
-let budget_one_day = +money/30;
-
-alert('Бюджет на 1 день = ' + budget_one_day.toFixed(2));
+let age = document.getElementById('age');
+ 
+function showUser(surname, name) {
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
-
-main();
+ 
+let a = showUser.bind(age);
+a('fsdf','fsdfsd');
