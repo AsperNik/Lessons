cadac373f35ffd37668b416264f44dbb9512d3cd
NikitaV <varfol007@gmail.com>
2019-05-13 20:43:39 +0300

    ff

---
+++ lesson12_compl/js/script.js
@@ -0,0 +1,31 @@
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd'),
+    data;
+
+inputRub.addEventListener('input', () => {
+    
+    let request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    
+    function exchangeRate() {
+        return new Promise(function (resolve, reject) {
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState === 4) {
+                    if (request.status == 200) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                }
+    });
+    });
+}
+exchangeRate()
+    .then( () => data = JSON.parse(request.response))
+    .then( () => inputUsd.value = inputRub.value / data.usd)
+    .catch( () => inputUsd.value = "Что-то пошло не так!")
+});
