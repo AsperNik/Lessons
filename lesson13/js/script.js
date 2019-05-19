7e63735f6f6ffc6af90be46ac90e7e1a45382b39
NikitaV <varfol007@gmail.com>
2019-05-19 18:55:49 +0300

    fix

--- lesson13/js/script.js
+++ lesson13/js/script.js
@@ -263,46 +263,48 @@ window.addEventListener('DOMContentLoaded', function(){
         totalValue = document.getElementById('total'),
         personsSum = 0,
         daysSum = 0,
-        total = 0;
+        total = 0,
+        koef = 1;
      // Валидация calc
 
-     for (let i = 0; i < counterInputs.length; i++){
+    for (let i = 0; i < counterInputs.length; i++){
         counterInputs[i].addEventListener('input',() => {
             counterInputs[i].value = counterInputs[i].value.replace(/[^0-9]/,'');
     });
     }
-        totalValue.innerHTML = '0';
+    totalValue.innerHTML = '0';
 
-        persons.addEventListener('change', function() {
-            personsSum = +this.value;
-            total = (daysSum + personsSum)*4000;
+    persons.addEventListener('input', function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum)*4000*koef;
 
-            if (restDays.value == '' || persons.value == '') {
-                totalValue.innerHTML = 0;
-            } else {
-                totalValue.innerHTML = total;
-            }
-        });
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
 
-        restDays.addEventListener('change', function() {
-            daysSum = +this.value;
-            total = (daysSum + personsSum)*4000;
+    restDays.addEventListener('input', function() {
+        daysSum = +this.value;
+        total = (daysSum + personsSum)*4000*koef;
 
-            if (persons.value == '' || restDays.value == '') {
-                totalValue.innerHTML = 0;
-            } else {
-                totalValue.innerHTML = total;
-            }
-        });
+        if (persons.value == '' || restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
 
-        place.addEventListener('change', function () {
-            if (restDays.value == '' || persons.value == '') {
-                totalValue.innerHTML = 0;
-            } else {
-                let a = total;
-                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
-            }
-        });
+    place.addEventListener('change', function () {
+        koef = this.options[this.selectedIndex].value;
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            total = (daysSum + personsSum)*4000*koef;
+            totalValue.innerHTML = total;
+        }
+    });
 
 
 });
